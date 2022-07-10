0724ba9a1089ee489384fe1f581ea6caf747f741
mslowin <slowinski.michal@outlook.com>
2022-07-10 21:55:38 +0200

    Dodaj pliki projektów.

---
+++ [algorytm 3] liczby strobogramatyczne/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _algorytm_3__liczby_strobogramatyczne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool run = true;
+
+            Console.WriteLine("The app checks if the number is stroboramatic. Press q to exit.");
+            
+            while(run)
+            {
+                string number = Console.ReadLine();
+                if(number == "q") // if the input is "q"
+                {
+                    run = false;
+                }
+                else if(int.TryParse(number, out int value)) // if the input is an intiger
+                {
+                    if(CheckNumber(number))
+                    {
+                        Console.WriteLine("The number IS stroboramatic :D");
+                    }
+                    else
+                    {
+                        Console.WriteLine("The number IS NOT stroboramatic :(");
+                    }
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("The input must be an intiger");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static bool CheckNumber(string number)
+        {
+            int counter = 0;
+            List<char> digits = new List<char>();
+            digits.AddRange(number);
+            for (int i = 0; i < number.Length; i++)
+            {
+                if((digits[i] == '8' && digits[(digits.Count - 1) - i] == '8') || (digits[i] == '1' && digits[(digits.Count - 1) - i] == '1') || 
+                    (digits[i] == '6' && digits[(digits.Count - 1) - i] == '9') || (digits[i] == '9' && digits[(digits.Count - 1) - i] == '6') ||
+                    (digits[i] == '2' && digits[(digits.Count - 1) - i] == '5') || (digits[i] == '5' && digits[(digits.Count - 1) - i] == '2'))
+                {
+                    counter++;
+                }
+            }
+            if(counter == number.Length)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+    }
+}
